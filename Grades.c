2b6215140dca58cb05bd28e4faf89665f00fc5ad
svskarishma <59288561+svskarishma@users.noreply.github.com>
2021-08-26 20:25:41 +0530

    Created

---
+++ Grades.c
@@ -0,0 +1,18 @@
+int main()
+    int marks;
+    printf("Enter your marks");
+    scanf("%d",&marks);
+    if (marks<=0&&marks>100)
+        printf("Wrong entry");
+    if (marks>=85&&marks<=100)
+        printf("Grade A");
+    else if(marks>=70&&marks<=84)
+        printf("Grade B");
+    else if(marks>=55&&marks<=69)
+        printf("Grade C");
+    else if(marks>=40&&marks<=54)
+        printf("Grade D");
+    else
+        printf("Grade F");
+
+    return 0;
