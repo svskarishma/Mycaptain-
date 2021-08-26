04f5a4a63da12d2d236b3f006c81aa8b9fd44a73
svskarishma <59288561+svskarishma@users.noreply.github.com>
2021-08-26 23:51:48 +0530

    created switch case.c

--- switch case.c
+++ switch case.c
@@ -1,21 +1,27 @@
+#include <stdio.h>
+
 int main()
 
-    printf("Enter a number from 1-5");
+    printf("Enter a number from 1-5\n");
+    int n;
     scanf("%d",&n);
-    switch(n)
+    switch (n)
 
         case 1:
-        printf("Food item-French fries\nPrice-Rs 99 ");break;
+        printf("Food item-French Fries\nPrice-Rs 99");break;
         case 2:
-        printf("Food item-Sandwich\nPrice-Rs 149 ");break;
+        printf("Food item-Burger\nPrice-Rs 129");break;
         case 3:
-        printf("Food item-French fries\nPrice-Rs 99 ");break;
+        printf("Food item-Sandwich\nPrice-Rs 149");break;
         case 4:
-        printf("Food item-French fries\nPrice-Rs 99 ");break;
+        printf("Food item-Pasta\nPrice-Rs 179");break;
         case 5:
-        printf("Food item-French fries\nPrice-Rs 99 ");break;
+        printf("Food item-Pizza\nPrice-Rs 239");break;
         default:
         printf("Enter a valid number");
+   
 
     return 0;
+
+
 
