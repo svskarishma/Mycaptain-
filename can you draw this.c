3f546e53941c285fedface487705072f8b5cce51
svskarishma <59288561+svskarishma@users.noreply.github.com>
2021-08-27 14:28:22 +0530

    created can you draw this.c
    the code prints 01 pattern

---
+++ can you draw this.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+  int n,i,j;
+  printf("Enter number of rows:");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=i;j<=1;j++)
+    {
+      if(j%2==1)
+      {
+        printf("0");
+      }
+      else
+      {
+        printf("1");
+      }
+    }
+    printf("\n");
+  }
+  return 0;
+}
+ 
